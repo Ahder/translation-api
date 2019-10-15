568bab746663ff0200bce829d53065f730e0f318
Ahder <benzinered@yahoo.fr>
2019-10-15 20:42:52 +0200

    Adding json test & fix delete.

--- src/main/java/com/rizomm/m2/tp1/traduction/entites/Entry.java
+++ src/main/java/com/rizomm/m2/tp1/traduction/entites/Entry.java
@@ -24,7 +24,7 @@ public class Entry {
     @NotBlank
     private String value;
 
-    @ManyToOne//(cascade = CascadeType.REMOVE)
+    @ManyToOne(cascade = CascadeType.REMOVE)
     @JsonIgnore
     private Translation translation;
 
